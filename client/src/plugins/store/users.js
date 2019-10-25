ff17c7d243fd019b0dac166b16a7725939bdaea8
unknown <lucas123.a17@gmail.com>
2019-10-25 16:32:31 +0200

    rename store file

---
+++ client/src/plugins/store/users.js
@@ -0,0 +1,121 @@
+import axios from 'axios'
+
+export default {
+  state: {
+    logged: false,
+    user: null
+  },
+  getters: {
+    isLogged (state) {
+      return state.logged
+    }
+  },
+  mutations: {
+    setLogged: (state, logged) => { state.logged = logged },
+    setUser: (state, user) => { state.user = user }
+  },
+  actions: {
+    getUser (store, id) {
+      if (id === undefined) {
+        return new Promise(function (resolve, reject) {
+          axios({
+            method: 'get',
+            url: store.getters.getApi + 'users?timestamp=' + new Date().getTime(),
+            responseType: 'json',
+            withCredentials: true
+          })
+            .then((response) => {
+              if (response.data.error === undefined) {
+                store.commit('setLogged', true)
+                store.commit('setUser', response.data)
+              } else {
+                store.commit('setLogged', false)
+                store.commit('setUser', null)
+              }
+              resolve(response.data)
+            })
+            .catch((error) => {
+              reject(error.response.data)
+            })
+        })
+      }
+      return new Promise(function (resolve, reject) {
+        axios({
+          method: 'get',
+          url: store.getters.getApi + 'users/' + id + '?timestamp=' + new Date().getTime(),
+          responseType: 'json',
+          withCredentials: true
+        })
+          .then((response) => {
+            resolve(response.data)
+          })
+          .catch((error) => {
+            reject(error.response.data)
+          })
+      })
+    },
+    login (store, { email, password }) {
+      return new Promise(function (resolve, reject) {
+        axios({
+          method: 'post',
+          url: store.getters.getApi + 'login?timestamp=' + new Date().getTime(),
+          responseType: 'json',
+          data: {
+            email: email,
+            password: password
+          },
+          withCredentials: true
+        })
+          .then((response) => {
+            if (response.data.error === undefined) {
+              store.commit('setLogged', true)
+              store.commit('setUser', response.data)
+            }
+            resolve(response.data)
+          })
+          .catch((error) => {
+            reject(error.response.data)
+          })
+      })
+    },
+    logout (store) {
+      return new Promise(function (resolve, reject) {
+        axios({
+          method: 'post',
+          url: store.getters.getApi + 'logout?timestamp=' + new Date().getTime(),
+          responseType: 'json',
+          withCredentials: true
+        })
+          .then((response) => {
+            store.commit('setLogged', false)
+            store.commit('setUser', null)
+            resolve(response.data)
+          })
+          .catch((error) => {
+            reject(error.response.data)
+          })
+      })
+    },
+    register (store, { email, pseudo, password }) {
+      return new Promise(function (resolve, reject) {
+        axios({
+          method: 'post',
+          url: store.getters.getApi + 'users?timestamp=' + new Date().getTime(),
+          responseType: 'json',
+          data: {
+            email: email,
+            pseudo: pseudo,
+            password: password
+          },
+          withCredentials: true
+        })
+          .then((response) => {
+            resolve(response.data)
+          })
+          .catch((error) => {
+            reject(error.response.data)
+          })
+      })
+    }
+  }
+}
