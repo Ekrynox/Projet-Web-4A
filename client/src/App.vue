289b969ff1cf14b926124ecc1d10cb8df0082c05
unknown <lucas123.a17@gmail.com>
2019-10-26 19:02:38 +0200

    Modify route Organisation

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,18 +5,40 @@
         <router-view />
       </v-container>
     </v-content>
+
+    <v-bottom-navigation v-if="$store.getters.isLogged" v-model="bottomNav" grow color="primary">
+      <v-btn value="msg" to="msg">
+        <span>Discutions</span>
+        <v-icon>mdi-android-messages</v-icon>
+      </v-btn>
+
+      <v-btn value="contacts" to="contacts">
+        <span>Contacts</span>
+        <v-icon>mdi-account-group</v-icon>
+      </v-btn>
+
+      <v-btn value="settings" to="settings">
+        <span>Settings</span>
+        <v-icon>mdi-settings</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
 <script>
 export default {
+  data: function () {
+    return {
+      bottomNav: 'msg'
+    }
+  },
   mounted: function () {
     // If not logged then go to the login page
     this.$store.dispatch('getUser').then((data) => {
       if (!this.$store.getters.isLogged) {
         this.$router.push('/login')
       } else {
-        this.$router.replace('/')
+        this.$router.replace('/msg')
       }
     })
   }
--- client/src/plugins/router.js
+++ client/src/plugins/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import HomePage from '../components/pages/HomePage'
+import ContactsPage from '../components/pages/ContactsPage'
 import LoginPage from '../components/pages/LoginPage'
 import RegisterPage from '../components/pages/RegisterPage'
 
@@ -9,7 +9,7 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    { path: '/', component: HomePage },
+    { path: '/contacts', component: ContactsPage },
     { path: '/login', component: LoginPage },
     { path: '/register', component: RegisterPage }
   ]
